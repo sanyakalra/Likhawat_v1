e61ea12e753a9d19791a24378321d3a0b5a56cf8
sanyak <sanyak@bitbucket.org>
2017-02-20 12:03:58 -0500

    Uploading of an Image

--- app/src/main/java/com/example/sanya/likhawat_v1/UploadActivity.java
+++ app/src/main/java/com/example/sanya/likhawat_v1/UploadActivity.java
@@ -1,9 +1,15 @@
 package com.example.sanya.likhawat_v1;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by sanya on 13/2/17.
@@ -11,7 +17,9 @@ import android.widget.Button;
 
 public class UploadActivity extends AppCompatActivity {
 
-    private Button btnUpload;
+    private ImageView viewImage;
+    private FloatingActionButton btnUpload;
+    private TextView btnContinue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +28,46 @@ public class UploadActivity extends AppCompatActivity {
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        });
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
             }
         });
     }
 
-    private void initializeIDs() {
-        btnUpload = (Button) findViewById(R.id.btn_upload);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String imagePath = cursor.getString(columnIndex);
+                cursor.close();
+                viewImage=(ImageView)findViewById(R.id.viewImage);
+                viewImage.setImageURI(selectedImage);
+                btnUpload.setVisibility(View.INVISIBLE);
+                btnContinue.setVisibility(View.VISIBLE);
+                Log.w("ImagePath*********", imagePath+"");
+
+            }
+        }
+    }
+
+    private void initializeIDs()
+    {
+        viewImage = (ImageView) findViewById(R.id.viewImage);
+        btnUpload = (FloatingActionButton) findViewById(R.id.btnUpload);
+        btnContinue = (TextView) findViewById(R.id.btnContinue);
     }
 }
