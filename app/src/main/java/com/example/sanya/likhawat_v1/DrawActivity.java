8d09f224eda0403bda4b719717345b9d75cdb2d8
sanyak <sanyak@bitbucket.org>
2017-02-20 22:14:59 -0500

    Writing text by hand using Canvas

--- app/src/main/java/com/example/sanya/likhawat_v1/DrawActivity.java
+++ app/src/main/java/com/example/sanya/likhawat_v1/DrawActivity.java
@@ -11,21 +11,33 @@ import android.widget.TextView;
 
 public class DrawActivity extends AppCompatActivity {
 
-    private TextView btnContinue;
+    private CanvasView drawingCanvas;
+    private TextView btnReset, btnContinue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload);
-        initializeIDs();
+        setContentView(R.layout.activity_draw);
+        initialiseIDs();
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawingCanvas.clearCanvas();
+            }
+        });
+
         btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
+
     }
 
-    private void initializeIDs() {
+    private void initialiseIDs(){
+        drawingCanvas = (CanvasView) findViewById(R.id.drawingCanvas);
+        btnReset = (TextView) findViewById(R.id.btnReset);
         btnContinue = (TextView) findViewById(R.id.btnContinue);
     }
 }
