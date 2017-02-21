110ae73b051f3e8fd59ecdf2070b7a1c26b2bef9
sanyakalra <skalra@bu.edu>
2017-02-20 22:18:18 -0500

    Adding of CanvasView.java

---
+++ app/src/main/java/com/example/sanya/likhawat_v1/CanvasView.java
@@ -0,0 +1,110 @@
+package com.example.sanya.likhawat_v1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by sanya on 20/2/17.
+ */
+public class CanvasView extends View {
+
+    public int width;
+    public int height;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    Context context;
+    private Paint mPaint;
+    private float mX, mY;
+    private static final float TOLERANCE = 5;
+
+    public CanvasView(Context c, AttributeSet attrs) {
+        super(c, attrs);
+        context = c;
+        mPath = new Path();
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(4f);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
+    @Override
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // draw the mPath with the mPaint on the canvas when onDraw
+        canvas.drawPath(mPath, mPaint);
+    }
+
+
+    private void startTouch(float x, float y) {
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+
+    }
+
+    private void moveTouch(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOLERANCE || dy >= TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+             mX = x;
+            mY = y;
+        }
+    }
+
+
+    public void clearCanvas() {
+        mPath.reset();
+        invalidate();
+
+    }
+
+    private void upTouch() {
+        mPath.lineTo(mX, mY);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startTouch(x, y);
+                invalidate();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                moveTouch(x, y);
+                invalidate();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                upTouch();
+                invalidate();
+                break;
+
+        }
+        return true;
+    }
+
+}
